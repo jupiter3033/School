23d773f0061bde2ad5615849068a791e34dcee58
jupiter <kacper.gadecki@teb.edu.pl>
2025-09-05 14:24:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//dodaj graczy, dodaj zespół, dodaj mecz, dodaj wynik meczów, print everything
+
+
+Console.WriteLine("┌───────────────────────────┐");
+Console.WriteLine("│                           │");
+Console.WriteLine("│   1.dodaj gracza          │");
+Console.WriteLine("│   2.usuń gracza           │");
+Console.WriteLine("│   3.pokaż liste meczów    │");
+Console.WriteLine("│   4.dodaj zespół          │");
+Console.WriteLine("│   5.usuń zespół           │");
+Console.WriteLine("│   6.stwórz mecz           │");
+Console.WriteLine("│   7.edytuj mecz           │");
+Console.WriteLine("│   8.zapisz                │");
+Console.WriteLine("│   9.wczytaj               │");
+Console.WriteLine("│   10.zakończ              │");
+Console.WriteLine("│                           │");
+Console.WriteLine("└───────────────────────────┘");
+Console.WriteLine("Wybierz opcje:");
+
+
+
