900ab96c7653c36e89b0bde63ae1a37e817c59a8
jupiter <kacper.gadecki@teb.edu.pl>
2025-09-12 14:22:44 +0200

    the comment update

--- UserInput.cs
+++ UserInput.cs
@@ -5,18 +5,18 @@
         AsInt32Start:
         try
         {
-            if(requestText != null)
+            if(requestText != null) 
             {
-                Console.Write(requestText);
+                Console.Write(requestText); //if request is not empty, proceed
             }
             string userInput = Console.ReadLine();
-            if (userInput == "" || userInput == null) goto AsInt32Start;
-            int pharsedInput = Int32.Parse(userInput);
+            if (userInput == "" || userInput == null) goto AsInt32Start;//check if user input is empty, if it is, return to start
+            int pharsedInput = Int32.Parse(userInput);//make user input a int32
             return pharsedInput;
         }
-        catch(Exception e)
+        catch(Exception e) 
         {
-            Console.WriteLine("To nie jest liczba!");
+            Console.WriteLine("To nie jest liczba!");//if its not a int, return to start
             goto AsInt32Start;
         }
     }
@@ -25,29 +25,29 @@
         AsStringStart:
         if (requestText != null)
         {
-            Console.Write(requestText);
+            Console.Write(requestText);//if request is not empty, proceed
         }
         string userInput = Console.ReadLine();
-        if (userInput == "" || userInput == null) goto AsStringStart;
+        if (userInput == "" || userInput == null) goto AsStringStart;//check if user input is empty, if it is, return to start
         return userInput;
     }
     public static float AsFloat(string requestText = null)
     {
         if (requestText != null)
         {
-            Console.Write(requestText);
+            Console.Write(requestText);//if request is not empty, proceed
         }
         AsFloatStart:
         try
         {
             string userInput = Console.ReadLine();
-            if (userInput == "" || userInput == null) goto AsFloatStart;
-            float pharsedInput = float.Parse(userInput);
+            if (userInput == "" || userInput == null) goto AsFloatStart;//check if user input is empty, if it is, return to start
+            float pharsedInput = float.Parse(userInput);//make user input a float
             return pharsedInput;
         }
         catch (Exception e)
         {
-            Console.WriteLine("To nie jest liczba!");
+            Console.WriteLine("To nie jest liczba!");//if its not a float, return to start
             goto AsFloatStart;
         }
     }
@@ -55,19 +55,19 @@
     {
         if (requestText != null)
         {
-            Console.Write(requestText);
+            Console.Write(requestText);//if request is not empty, proceed
         }
         AsDateTime:
         try
         {
             string userInput = Console.ReadLine();
-            if (userInput == "" || userInput == null) goto AsDateTime;
-            DateTime pharsedInput = DateTime.Parse(userInput);
+            if (userInput == "" || userInput == null) goto AsDateTime;//check if user input is empty, if it is, return to start
+            DateTime pharsedInput = DateTime.Parse(userInput);// make user input date and time format
             return pharsedInput;
         }
         catch (Exception e)
         {
-            Console.WriteLine("To nie poprawny format daty!\nFormat: \"08/18/2018 07:22:16\"");
+            Console.WriteLine("To nie poprawny format daty!\nFormat: \"08/18/2018 07:22:16\"");//if its not a correct date and time format, return to start
             goto AsDateTime;
         }
     }
