1803ccd4bbe89755cce2cdc3180a5bcdb7c1f543
jupiter <kacper.gadecki@teb.edu.pl>
2025-09-17 23:55:51 +0200

    the comment update

--- Program.cs
+++ Program.cs
@@ -1,73 +1,77 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 
 namespace SportOrganizer;
 
 class Program
 {
-    public static List<Player> playerList = new List<Player>();
-    public static List<Match> matchList = new List<Match>();
-    public static List<Team> teamList = new List<Team>();
+    public static List<Player> playerList = new List<Player>(); // list of players
+    public static List<Match> matchList = new List<Match>(); // list of matches
+    public static List<Team> teamList = new List<Team>(); // list of teams
+
     static void Main(string[] args)
     {
         StartOfMain:
-        GC.Collect(); //Becouse i can B)
-        TUI.PrintMainMenu();
-        int UserChoice = UserInput.AsInt32();
+        GC.Collect();
+        TUI.PrintMainMenu(); // print main menu
+        int UserChoice = UserInput.AsInt32(); // get user input
+
         switch(UserChoice)
         {
             default:
-                goto StartOfMain;
+                goto StartOfMain; // go back to main menu if input is invalid
+
             case 1: // add player
                 Player player = new Player();
                 player.name = UserInput.AsString("Podaj imię gracza: ");
                 player.surname = UserInput.AsString("Podaj nazwisko gracza: ");
                 player.age = UserInput.AsInt32("Podaj wiek gracza: ");
                 player.kdr = UserInput.AsFloat("Podaj KDR gracza: ");
-                playerList.Add(player);
+                playerList.Add(player); // add player to list
                 goto StartOfMain;
+
             case 2: // remove player
                 if (playerList.Count != 0)
                 {
-                    PrintAllPlayers(playerList);
+                    PrintAllPlayers(playerList); // show all players
                     int indexToRemove = UserInput.AsInt32("Podaj index do usunięcia: ");
                     try
                     {
-                        playerList.RemoveAt(indexToRemove-1);
-                    }
+                        playerList.RemoveAt(indexToRemove-1); // remove by index
                     catch(Exception ex)
                     {
-                        Console.WriteLine("Coś poszło nie tak... : "+ex);
+                        Console.WriteLine("Coś poszło nie tak... : "+ex); // error message if index is invalid
                     }
                 }
                 else
                     Console.WriteLine("Nie ma co usunąć!");
                 
                 goto StartOfMain;
+
             case 4: // add team
                 Team team = new Team();
                 int i = 0;
-                while (i != -1)
+                while (i != -1) // loop for adding players to the team
                 {
                     Console.WriteLine("Podaj indexy graczy ktrórych dodać do drużyny.\n(wpisz 0 aby pokazać wszystkich dostępnych graczy, lub -1 aby zakończyć wpisywanie.)");
                     int userInput = UserInput.AsInt32(">");
                     try
                     {
-                        if (userInput > 0) //Index Given
+                        if (userInput > 0) // player index was given
                         {
-                            if (team.playersIndexes.Contains(userInput - 1))
+                            if (team.playersIndexes.Contains(userInput - 1)) // check if player already in team
                             {
                                 Console.WriteLine("Ten gracz jest już w tej drużynie!");
                             }
                             else
                             {
                                 Console.WriteLine("Dodano : " + playerList[userInput - 1].name);
-                                team.playersIndexes.Add(userInput - 1);
+                                team.playersIndexes.Add(userInput - 1); // add player index to team
                             }
                         }
-                        else // Special Index
+                        else
                         {
-                            if (userInput == 0) PrintAllPlayers(playerList);
-                            if (userInput == -1) break;
+                            if (userInput == 0) PrintAllPlayers(playerList); // show players
+                            if (userInput == -1) break; // finish adding players
                         }
                     }
                     catch
@@ -81,61 +85,57 @@ class Program
                 team.gamesLost = UserInput.AsInt32("Podaj ilość gier przegranych: ");
                 team.gamesTied = UserInput.AsInt32("Podaj ilość gier zakończonych remisem: ");
                 team.captainName = UserInput.AsString("Podaj imię kapitana: ");
-                teamList.Add(team);
+                teamList.Add(team); // add team to list
                 goto StartOfMain;
+
             case 6: // create match
                 Match match = new Match();
                 
                 try
                 {
-
                     match.teamOneindex = UserInput.AsInt32("Index Drużuny jeden: ");
-                    Team readTest = teamList[match.teamOneindex]; //will crash if team does not exist and thats the point. :P
+                    Team readTest = teamList[match.teamOneindex]; // check if team exists
                     match.teamTwoindex = UserInput.AsInt32("Index Drużuny dwa: ");
-                    readTest = teamList[match.teamTwoindex]; // same here
+                    readTest = teamList[match.teamTwoindex]; // check if second team exists
                     match.teamOnePoints = UserInput.AsInt32("Punkty Drużuny jeden: ");
                     match.teamTwoPoints = UserInput.AsInt32("Punkty Drużuny dwa: ");
                     match.startOfMatch = UserInput.AsDateTime("Godzina rozpoczęcia meczu :");
                     match.matchTime = UserInput.AsFloat("Czas trwania meczu: ");
                     match.teamWon = UserInput.AsString("Drużyna Która wygrała (Może być nikt): ");
-                    readTest = null; // to be eaten by GC.
+                    readTest = null;
                 }
                 catch
                 {
                     Console.WriteLine("Coś poszło nie tak... czy ten gracz istnieje? (czy to poprawny index?)");
                 }
-                matchList.Add(match);
-
-                /*public int teamOnePoints;
-			public int teamTwoPoints;
-			public int teamOneindex;
-			public int teamTwoindex;
-			public DateTime startOfMatch;
-			public float matchTime;
-			public string teamWon;
-	}*/
-
+                matchList.Add(match); // add match to list
                 goto StartOfMain;
-            case 8:
-                thesaveandload.thesave(); // WHY DO THEY NEED THE 'the' PART WTF KAPER
+
+            case 8: // save data
+                thesaveandload.thesave();
                 goto StartOfMain;
-            case 9:
+
+            case 9: // load data
                 thesaveandload.theload();
                 goto StartOfMain;
-            case 10: // exit
+
+            case 10: // exit program
                 break;
-            case 11: // show all
+
+            case 11: // show all players and teams
                 PrintAllPlayers(playerList);
                 PrintAllTeams(teamList);
                 goto StartOfMain;
-            case 12: // clear
+
+            case 12: // clear console
                 Console.Clear();
                 goto StartOfMain;
         }
     }
+
     static void PrintAllPlayers(List<Player> _playerList)
     {
-        if (_playerList.Count != 0) // playerz
+        if (_playerList.Count != 0)
         {
             Console.WriteLine("Gracze: ");
             int i = 1;
@@ -146,9 +146,10 @@ class Program
             }
         }
     }
+
     static void PrintAllTeams(List<Team> _teamList)
     {
-        if (_teamList.Count != 0) // teams
+        if (_teamList.Count != 0)
         {
             Console.WriteLine("Drużyny: ");
             int i = 1;
@@ -166,8 +167,3 @@ class Program
         }
     }
 }
-
-
-
-
-
