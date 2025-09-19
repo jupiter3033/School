50eef03cf30a7b214289f1f176a43dd9f21247a4
Barrer <102591557+Barrer898@users.noreply.github.com>
2025-09-19 15:49:20 +0200

    the comment update

--- thesaveandload.cs
+++ thesaveandload.cs
@@ -9,22 +9,22 @@ namespace SportOrganizer
 		public static void thesave()
 		{
             Console.Write("podaj nazwe pliku do zapisu:");
-            string path = Console.ReadLine();
+            string path = Console.ReadLine();//give the name pls
 
 			if (string.IsNullOrWhiteSpace(path))
-				path= "thelist.txt";
+				path= "thelist.txt";//if didnt give the name make it deafult
 
             using (StreamWriter Writer = new StreamWriter(path))
 			{
-				Writer.WriteLine("[Players]");
+				Writer.WriteLine("[Players]");//the lable for players
 				foreach(Player _player in Program.playerList)
-					Writer.WriteLine($"{_player.name}|{_player.surname}|{_player.age}|{_player.kdr}");
+					Writer.WriteLine($"{_player.name}|{_player.surname}|{_player.age}|{_player.kdr}");//save the info pls
 
-                Writer.WriteLine("[Match]");
+                Writer.WriteLine("[Match]");//the lable for matches
                 foreach (Match _match in Program.matchList)
-                    Writer.WriteLine($"{_match.teamOnePoints}|{_match.teamTwoPoints}|{_match.teamOneindex}|{_match.teamTwoindex}|{_match.startOfMatch}|{_match.matchTime}|{_match.teamWon}");
+                    Writer.WriteLine($"{_match.teamOnePoints}|{_match.teamTwoPoints}|{_match.teamOneindex}|{_match.teamTwoindex}|{_match.startOfMatch}|{_match.matchTime}|{_match.teamWon}");//safe the info pls
 
-                Writer.WriteLine("[Team]");
+                Writer.WriteLine("[Team]");//the lable for the teams
                 foreach (Team _team in Program.teamList)
                 {
                     string convertedPlayersIndexes = "";
@@ -33,25 +33,25 @@ namespace SportOrganizer
                         convertedPlayersIndexes = convertedPlayersIndexes + Convert.ToString(playerIndex) + "\\";
                     }
                     
-                    Writer.WriteLine($"{convertedPlayersIndexes}|{_team.teamName}|{_team.gamesWon}|{_team.gamesLost}|{_team.gamesTied}|{_team.captainName}");
+                    Writer.WriteLine($"{convertedPlayersIndexes}|{_team.teamName}|{_team.gamesWon}|{_team.gamesLost}|{_team.gamesTied}|{_team.captainName}");//safe the info pls
                 }
 
                 Console.WriteLine($"zapisano dane do pliku: {path}");
             }
         }
-		public static void theload()
+		public static void theload()//DunDun
 		{
             Program.matchList.Clear();
-            Program.playerList.Clear();
+            Program.playerList.Clear();//clean THE lists so we dont merge the data with the old ones
             Program.teamList.Clear();
 
-            string loadpath = UserInput.AsString("podaj nazwe pliku do załadowania: ");
+            string loadpath = UserInput.AsString("podaj nazwe pliku do załadowania: ");//give the loadpath pls
 
             using(StreamReader Reader = new StreamReader(loadpath))
 			{
                 string line;
                 string readType=null;
-                while ((line = Reader.ReadLine()) != null)
+                while ((line = Reader.ReadLine()) != null)//when not nothing procced
                 {
                     System.Console.WriteLine(line);
                     if (line == "[Players]") readType = "Player";
@@ -93,7 +93,7 @@ namespace SportOrganizer
                             if (!(line == "[Team]")) // ignore if this is a label line
                             {
                                 Team team = new Team();
-                                string[] splitString = line.Split('|');
+                                string[] splitString = line.Split('|');//split everything so its more easy to read ig
                                 string[] splitPlayerIndexes = splitString[0].Split('\\');
                                 for ( int i = 0; i< splitPlayerIndexes.Length;i++) // playerIndexes
                                 {
@@ -102,7 +102,7 @@ namespace SportOrganizer
                                         Console.WriteLine(splitPlayerIndexes.Length);
                                         Console.WriteLine();
                                         Console.WriteLine(i);
-                                        team.playersIndexes.Add(int.Parse(splitPlayerIndexes[i])); // god
+                                        team.playersIndexes.Add(int.Parse(splitPlayerIndexes[i])); // god ... ah yes god
                                     }
                                 }
                                 team.teamName = splitString[1];
